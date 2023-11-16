b909af2a70815947d212cfa01451cdcab5b0f984
Александр Лапин <alexlapin.ds@gmail.com>
2023-11-17 01:05:31 +0300

    added test dir with test files and some example tests

---
+++ tests/test_backend.py
@@ -0,0 +1,23 @@
+def test_register_user(client):
+    """
+    Test successful user registration
+    :param client:
+    :return:
+    """
+    response = client.post("/register",
+                           json={"username": "testuser", "password": "testpassword", "email": "test@example.com",
+                                 "phone": "1234567890", "country": "Testland"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "User registered successfully"}
+
+
+def test_login_user(client):
+    """
+    Test successful user registration
+    :param client:
+    :return:
+    """
+    response = client.post("/login", json={"email": "test@example.com", "password": "testpassword"})
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+    assert response.json()["token_type"] == "bearer"
