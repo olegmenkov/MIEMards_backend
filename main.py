ba2e4217d33eb8b6caece5ce35ca3a5591b4f985
Oleg Menkov <93485639+olegmenkov@users.noreply.github.com>
2023-11-23 23:18:45 +0300

    added cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 import ai
 from ai.generate_image import generate_image
@@ -15,6 +16,21 @@ from schemas import *
 # TODO: протестить
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/register")
