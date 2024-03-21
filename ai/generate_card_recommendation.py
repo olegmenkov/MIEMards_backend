f710253f00096856a5bdce3fcfc50bc44dc88b05
Александр Лапин <alexlapin.ds@gmail.com>
2024-03-22 02:45:34 +0300

    fixed promt

--- ai/generate_card_recommendation.py
+++ ai/generate_card_recommendation.py
@@ -5,9 +5,13 @@ from loguru import logger
 
 
 def generate_card_recommendation(topic, existing_words):
-    system_prompt = """Based on the provided list of words, suggest a new word and its Russian translation that is contextually relevant to them.
-    Example: hammer молоток"""
-    user_template = "Given these words in the card deck: {words}, and topic {topic} suggest a new relevant word and its Russian translation."
+    system_prompt = """Based on the provided list of words and topic, suggest a new word and its Russian translation that is contextually relevant to them. Return 2 words with space
+    Example 1:
+    hammer молоток
+    Example 2:
+    phone телефон
+    """
+    user_template = "Given topic {topic} and these words in the card deck: {words}, suggest a new relevant word and its Russian translation."
     words_string = ', '.join(existing_words)
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125")
     chat_template = ChatPromptTemplate.from_messages([SystemMessage(content=system_prompt),
