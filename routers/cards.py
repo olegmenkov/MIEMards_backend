81f8b4e753730fdfafd92c5655856b319fe67431
Александр Лапин <alexlapin.ds@gmail.com>
2024-03-22 01:47:30 +0300

    починил хуйню

--- routers/cards.py
+++ routers/cards.py
@@ -112,7 +112,7 @@ async def generate_image(word: str, user_id: str = Depends(authentification.get_
         raise HTTPException(status_code=404, detail="User not found")
 
     # mock
-    return JSONResponse(content={'url': "faketaxi"})
+    return JSONResponse(content={'url': "https://images-assets-ht.project1content.com/Fakehub/Tour/FakeTaxi/Categories/5da0a232d0d0e6.33516581.jpg"})
 
     url = ai.generate_image.generate_image(word)
     return JSONResponse(content={'url': url})
