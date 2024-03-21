a4362c8349c93a3e19319c1f80390ce681ba97ba
Александр Лапин <alexlapin.ds@gmail.com>
2024-03-22 02:39:56 +0300

    fixed output

--- ai/generate_card_recommendation.py
+++ ai/generate_card_recommendation.py
@@ -5,7 +5,8 @@ from loguru import logger
 
 
 def generate_card_recommendation(topic, existing_words):
-    system_prompt = "Based on the provided list of words, suggest a new word and its Russian translation that is contextually relevant to them."
+    system_prompt = """Based on the provided list of words, suggest a new word and its Russian translation that is contextually relevant to them.
+    Example: hammer молоток"""
     user_template = "Given these words in the card deck: {words}, and topic {topic} suggest a new relevant word and its Russian translation."
     words_string = ', '.join(existing_words)
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125")
@@ -14,14 +15,7 @@ def generate_card_recommendation(topic, existing_words):
     formatted_template = chat_template.format_messages(words=words_string, topic=topic)
     message = llm.invoke(formatted_template).content.strip()
 
-    suggested_word_pair = message.split('\n')
-    word_dict = {}
-
-    if len(suggested_word_pair) == 2:
-        english_word = suggested_word_pair[0].split(': ')[1]
-        russian_word = suggested_word_pair[1].split(': ')[1].split(' ')[0]
-        word_dict[english_word] = russian_word
-
+    suggested_word_pair = message.split(' ')
+    logger.info(suggested_word_pair)
     logger.info('Рекомендация карт сгенерирована!')
-    logger.debug(word_dict)
-    return list(word_dict.keys())[0], list(word_dict.values())[0]
+    return suggested_word_pair[0], suggested_word_pair[1]
